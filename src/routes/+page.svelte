c7305c48feb0cc45268426d457d7e732eee35b2c
Elodin77 <developerdjay@gmail.com>
2023-10-01 23:47:34 +0800

    made a circle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(async () => {
+    const { createGlobe } = await import('$lib/globe.js');
+    createGlobe();
+  }); 
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
