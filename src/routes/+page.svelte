7e13dcc6167ea3a4fa3ea4f6b57bc522a76cdbec
Djimon <developerdjay@gmail.com>
2023-10-22 12:59:22 +0800

    sass

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,7 @@
 <script>
-  import { onMount } from 'svelte';
 
-  onMount(async () => {
-    const { createGlobe } = await import('$lib/globe.js');
-    createGlobe();
-  }); 
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div id="globe"></div>
